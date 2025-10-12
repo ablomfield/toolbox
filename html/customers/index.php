94cb07a2307d4be24da6dd91da1b518d1151ee03
Adam Blomfield <adam@blomfield.me>
2025-10-11 21:47:18 -0500

    cust

--- html/customers/index.php
+++ html/customers/index.php
@@ -41,6 +41,63 @@ if (isset($_SESSION['authtoken'])) {
 						<div class="box post">
 							<h1>Select Customer:</h1>
 							<?php
+							// Retrieve Org List
+							$orgsurl = "https://webexapis.com/v1/organizations";
+							$getorgs = curl_init($orgsurl);
+							curl_setopt($getorgs, CURLOPT_CUSTOMREQUEST, "GET");
+							curl_setopt($getorgs, CURLOPT_RETURNTRANSFER, true);
+							curl_setopt($getorgs, CURLOPT_FAILONERROR, true);
+							curl_setopt(
+								$getorgs,
+								CURLOPT_HTTPHEADER,
+								array(
+									'Content-Type: application/json',
+									'Authorization: Bearer ' . $authtoken
+								)
+							);
+							$orgsdata = curl_exec($getorgs);
+							if (curl_errno($getorgs) == "0") {
+								$orgsjson = json_decode($orgsdata);
+								$orgsarray = json_decode($orgsdata, true);
+								$orgcount = count($orgsarray['items']);
+								echo ("					  <p>Select an organization to search for trunks:</p>\n");
+								echo ("					  <form method=\"post\" action=\"/lgw/\">\n");
+								echo ("					    <input type=\"hidden\" name=\"lgwstep\" value=\"2\">\n");
+								echo ("					    <table class=\"default\">\n");
+								for ($x = 0; $x < $orgcount; $x++) {
+									echo ("					      <tr>\n");
+									echo ("					        <td>\n");
+									echo ("     					    <label class=\"radio-container\">\n");
+									echo ("			     		        <input type=\"radio\" name=\"orgid\" value=\"" . $orgsjson->items[$x]->id . "\">\n");
+									echo ("					            <span class=\"radio-checkmark\"></span>\n");
+									echo ("					        </td>\n");
+									echo ("					        <td>\n");
+									echo ("					         " . $orgsjson->items[$x]->displayName . "</label>\n");
+									echo ("					        </td>\n");
+									echo ("					      </tr>\n");
+								}
+								echo ("					      <tr>\n");
+								echo ("					        <td colspan=\"2\">\n");
+								echo ("					          <input type=\"submit\" value=\"Continue\" class=\"button\"><br/>\n");
+								echo ("					        </td>\n");
+								echo ("					      </tr>\n");
+								echo ("					    </table>\n");
+								echo ("					  </form>\n");
+							} else {
+								echo "					  <p>Sorry, you don't have access to any organizations.</p>\n";
+							}
+							echo ("           <table class=\"default\">\n");
+							echo ("             <tr>\n");
+							echo ("					      <form method=\"post\">\n");
+							echo ("					      <input type=\"hidden\" name=\"lgwstep\" value=\"" . ($lgwstep - 1) . "\">\n");
+							echo ("					      <td><input type=\"submit\" value=\"Go Back\" class=\"button\"></td>\n");
+							echo ("					      </form>\n");
+							echo ("					      <form method=\"post\" action=\"/lgw/logout\">\n");
+							echo ("					      <td><input type=\"submit\" value=\"Start Over\" class=\"button\"></td>\n");
+							echo ("					      </form>\n");
+							echo ("             </tr>\n");
+							echo ("           </table>\n");
+							?>
 							?>
 						</div>
 					</section>

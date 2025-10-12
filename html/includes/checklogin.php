ec3a879b12bc268063ea7cc909161536bac0e899
Adam Blomfield <adam@blomfield.me>
2025-10-11 23:06:36 -0500

    checklogin

--- html/includes/checklogin.php
+++ html/includes/checklogin.php
@@ -1,17 +1,22 @@
 <?php
 if (isset($_SESSION['authtoken'])) {
-	$loggedin = True;
-	$displayname = $_SESSION["displayname"];
-	$authtoken = $_SESSION["authtoken"];
-	if (isset($_SESSION['orgname'])) {
-		$orgname = $_SESSION["orgname"];
-	} else {
-		$orgname = "";
-	}
-	$isadmin = $_SESSION["isadmin"];
-	$timezone = $_SESSION["timezone"];
-	$email = $_SESSION["email"];
+    $loggedin = True;
+    $displayname = $_SESSION["displayname"];
+    $authtoken = $_SESSION["authtoken"];
+    if (isset($_SESSION['orgname'])) {
+        $orgname = $_SESSION["orgname"];
+    } else {
+        $orgname = "";
+    }
+    if (isset($_SESSION['enabledebug'])) {
+        $enabledebug = $_SESSION["enabledebug"];
+    } else {
+        $enabledebug = false;
+    }
+
+    $isadmin = $_SESSION["isadmin"];
+    $timezone = $_SESSION["timezone"];
+    $email = $_SESSION["email"];
 } else {
-	$loggedin = False;
+    $loggedin = False;
 }
-?>

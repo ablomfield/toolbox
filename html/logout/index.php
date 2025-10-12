fbf20a1200073986c8368188eaf211f4888f30c0
Adam Blomfield <adam@blomfield.me>
2025-10-11 21:13:24 -0500

    logout

--- html/logout/index.php
+++ html/logout/index.php
@@ -1,4 +1,14 @@
 <?php
+// Import Settings
+include($_SERVER['DOCUMENT_ROOT'] . "/includes/settings.php");
+
+// Write History
+if (isset($_SESSION["email"])) {
+    $email = $_SESSION["email"];
+    mysqli_query($dbconn, "INSERT INTO history (eventdate, eventsource, eventdesc) VALUES(NOW(),'" . $email . "','LOGOUT')");
+}
+
+// Log Out
 session_start();
 session_destroy();
 header('Location: /');

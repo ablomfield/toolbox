85b7299f1c4bf52a813e7be972dda792f527812b
Adam Blomfield <adam@blomfield.me>
2025-10-12 12:08:59 -0500

    activate

--- html/tools/activate/steps/0.php
+++ html/tools/activate/steps/0.php
@@ -2,5 +2,5 @@
 	<p>This tool is used to activate or deactivate numbers in the Webex Calling dial plan.</p>
 	<p>You will need to select the location to edit numbers, choose whether to activate or deactivate, and provide a list of up to 500 numbers (one numberper row).</p>
 	<p>
-	<form method="post"><input type="hidden" name="lgwstep" value="1"><input type="submit" value="Continue"></form>
+	<form method="post"><input type="hidden" name="toolstep" value="1"><input type="submit" value="Continue"></form>
 	</p>

ba4fb745eb92bfb75504fa688752116da32076eb
Adam Blomfield <adam@blomfield.me>
2025-10-11 22:16:25 -0500

    css

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -2,7 +2,7 @@
 					<!-- Copyright -->
 						<div class="copyright">
 							<ul class="menu">
-								<li>Copyright <a href="/session"  style="text-decoration:none; border-bottom: none; transition: none;">©</a> <?php echo (date('Y')); ?> <a href="mailto:ablomfield@presidio.com">Adam Blomfield</a>. All rights reserved.</li>
+								<li>Copyright <a href="/session"  clas="nodec">©</a> <?php echo (date('Y')); ?> <a href="mailto:ablomfield@presidio.com">Adam Blomfield</a>. All rights reserved.</li>
 							</ul>
 						</div>
 				</div>

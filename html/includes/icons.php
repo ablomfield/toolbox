4e54e2322fc8b4a805db226345033d3623323ae2
Adam Blomfield <adam@blomfield.me>
2025-10-12 08:43:19 -0500

    icons

--- html/includes/icons.php
+++ html/includes/icons.php
@@ -4,15 +4,21 @@
 					<section class="col-6 col-12-narrower">
 						<div class="box highlight">
 							<i class="icon major solid fa-network-wired" style="text-decoration: none;"></i>
-							<h3><a href="/lgw">LGW Config Generator</a></h3>
+							<h3><a href="/tools/lgw">LGW Config Generator</a></h3>
 						</div>
 					</section>
 					<section class="col-6 col-12-narrower">
 						<div class="box highlight">
 							<i class="icon major solid fa-network-wired" style="text-decoration: none;"></i>
-							<h3><a href="/lgw">Analog Gateway Tool</a></h3>
+							<h3><a href="/tools/analog">Analog Gateway Tool</a></h3>
 						</div>
 					</section>
+					<section class="col-6 col-12-narrower">
+						<div class="box highlight">
+							<i class="icon major solid fa-hashtag" style="text-decoration: none;"></i>
+							<h3><a href="/tools/activate">Activate Numbers</a></h3>
+						</div>
+					</section>					
 				</div>
 			</div>
 		</section>

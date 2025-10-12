a77551e5951270b29af359cffba9584f8b4dd328
Adam Blomfield <adam@blomfield.me>
2025-10-12 15:45:55 -0500

    fwd

--- html/tools/forward/steps/2.php
+++ html/tools/forward/steps/2.php
@@ -20,36 +20,57 @@ $putjson = "{\"callForwarding\":{\"always\":{\"enabled\":false}}}";
 
 // Unforward Users
 $usercount = count($emailarr);
-echo ("Attempting to unforward $usercount user(s)...<br>\n");
-for ($x = 0; $x < $usercount; $x++) {
-  //echo ("Checking $emailarr[$x].<br />\n");
-  $personid = webexgetpersonid($authtoken, $emailarr[$x]);
-  if ($personid != NULL) {
-    $fwdurl = "https://webexapis.com/people/$personid/features/callForwarding?orgId=$orgid";
-    $putfwd = curl_init($fwdurl);
-    curl_setopt($putfwd, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_setopt($putfwd, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($putfwd, CURLOPT_POSTFIELDS, $putjson);
-    curl_setopt(
-      $putfwd,
-      CURLOPT_HTTPHEADER,
-      array(
-        'Content-Type: application/json',
-        'Authorization: Bearer ' . $authtoken
-      )
-    );
-    $fwddata = curl_exec($putfwd);
-    $fwdjson = json_decode($fwddata);
-    $fwdarray = json_decode($fwddata, true);
+if ($usercount > 0) {
+  echo ("Attempting to unforward $usercount user(s)...<br>\n");
+  echo ("<table class=\"default\">\n");
+  echo ("<thead>\n");
+  echo ("<tr>\n");
+  echo ("<th>User</th>\n");
+  echo ("<th>Result</th>\n");
+  echo ("</tr>\n");
+  echo ("</thead>\n");
+  echo ("<tbody>\n");
+  for ($x = 0; $x < $usercount; $x++) {
+    //echo ("Checking $emailarr[$x].<br />\n");
+    $personid = webexgetpersonid($authtoken, $emailarr[$x]);
+    if ($personid != NULL) {
+      $fwdurl = "https://webexapis.com/people/$personid/features/callForwarding?orgId=$orgid";
+      $putfwd = curl_init($fwdurl);
+      curl_setopt($putfwd, CURLOPT_CUSTOMREQUEST, "PUT");
+      curl_setopt($putfwd, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($putfwd, CURLOPT_POSTFIELDS, $putjson);
+      curl_setopt(
+        $putfwd,
+        CURLOPT_HTTPHEADER,
+        array(
+          'Content-Type: application/json',
+          'Authorization: Bearer ' . $authtoken
+        )
+      );
+      $fwddata = curl_exec($putfwd);
+      $fwdjson = json_decode($fwddata);
+      $fwdarray = json_decode($fwddata, true);
 
-    if (curl_getinfo($putfwd, CURLINFO_HTTP_CODE) == "204") {
-      echo ("$emailarr[$x] - Successfully unforwarded.<br />\n");
+      if (curl_getinfo($putfwd, CURLINFO_HTTP_CODE) == "204") {
+        echo ("<tr>\n");
+        echo ("<td>$emailarr[$x]</td>\n");
+        echo ("<td>Successful</td>\n");
+        echo ("</tr>\n");
+      } else {
+        echo ("<tr>\n");
+        echo ("<td>$emailarr[$x]</td>\n");
+        echo ("<td>Failed</td>\n");
+        echo ("</tr>\n");
+      }
     } else {
-      echo ("$emailarr[$x] - Unforwarded failed.<br />\n");
+      echo ("<tr>\n");
+      echo ("<td>$emailarr[$x]</td>\n");
+      echo ("<td>Not Found</td>\n");
+      echo ("</tr>\n");
     }
-  } else {
-    echo ("$emailarr[$x] - User not found.<br />\n");
   }
+  echo ("</tbody>\n");
+  echo ("</table>\n");
 }
 echo ("           <table class=\"default\">\n");
 echo ("             <tr>\n");

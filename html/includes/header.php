296560cdff101408c05b1f7bf5f3769890a584f2
Adam Blomfield <adam@blomfield.me>
2025-10-11 21:03:55 -0500

    header

--- html/includes/header.php
+++ html/includes/header.php
@@ -1,15 +1,20 @@
 				<div id="header">
 					<!-- Logo -->
-					<h1><a href="/"><img height="75px" src="/images/icononly_transparent_nobuffer.png" alt="<?php echo($sitetitle); ?>"></a>&nbsp;&nbsp;&nbsp;<a href="/" id="logo"><?php echo($sitetitle); ?></a></h1>
+					<h1><a href="/"><img height="75px" src="/images/icononly_transparent_nobuffer.png" alt="<?php echo ($sitetitle); ?>"></a>&nbsp;&nbsp;&nbsp;<a href="/" id="logo"><?php echo ($sitetitle); ?></a></h1>
 					<!-- Nav -->
-					<nav id="nav">
-						<ul>
-							<li<?php if ($sitesec == "home"){ echo(" class=\"current\""); } ?>><a href="/">Home</a></li>
-							<li<?php if ($sitesec == "tools"){ echo(" class=\"current\""); } ?>><a href="#">Tools</a>
-								<ul>
-									<li><a href="/lgw">LGW Generator</a></li>
-								</ul>
-							</li>
-						</ul>
-					</nav>
+					echo(" <nav id=\"nav\">\n");
+						echo(" <ul>\n");
+							echo(" <li");
+								if ($sitesec==\"home\"){ echo(\" class=\\"current\\"\"); }
+								echo("><a href=\"/\">Home</a></li>\n");
+								echo(" <li");
+									if ($sitesec==\"tools\"){ echo(\" class=\\"current\\"\"); }
+									echo("><a href=\"#\">Tools</a>\n");
+									echo(" <ul>\n");
+										echo(" <li><a href=\"/lgw\">LGW Generator</a></li>\n");
+										echo(" </ul>\n");
+									echo(" </li>\n");
+									echo(" <li><a href=\"/logout\">Logout</a></li>\n");
+									echo(" </ul>\n");
+						echo(" </nav>\n");
 				</div>

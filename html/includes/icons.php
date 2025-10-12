150a869b8372810735b4f04b19b2c363148138a9
Adam Blomfield <adam@blomfield.me>
2025-10-12 09:40:15 -0500

    icons

--- html/includes/icons.php
+++ html/includes/icons.php
@@ -1,24 +1,19 @@
 		<section class="wrapper style1">
 			<div class="container">
-				<div class="row gtr-200">
-					<section class="col-6 col-12-narrower">
-						<div class="box highlight">
-							<i class="icon major solid fa-network-wired" style="text-decoration: none;"></i>
-							<h3><a href="/tools/lgw">LGW Config Generator</a></h3>
-						</div>
-					</section>
-					<section class="col-6 col-12-narrower">
-						<div class="box highlight">
-							<i class="icon major solid fa-network-wired" style="text-decoration: none;"></i>
-							<h3><a href="/tools/analog">Analog Gateway Tool</a></h3>
-						</div>
-					</section>
-					<section class="col-6 col-12-narrower">
-						<div class="box highlight">
-							<i class="icon major solid fa-hashtag" style="text-decoration: none;"></i>
-							<h3><a href="/tools/activate">Activate Numbers</a></h3>
-						</div>
-					</section>					
-				</div>
+				<div class="row gtr-200"><?php
+											$rsdata = mysqli_query($dbconn, "SELECT pkid, name, isactive FROM tools ORDER BY name") or die("Error in Selecting " . mysqli_error($dbconn));
+											if ($rsdata) {
+												if (mysqli_num_rows($rsdata) > 0) {
+													while ($row = mysqli_fetch_assoc($rsdata)) {
+														echo ("					<section class=\"col-6 col-12-narrower\">\n");
+														echo ("						<div class=\"box highlight\">\n");
+														echo ("							<i class=\"icon major solid " . $row["icon"] . "\" style=\"text-decoration: none;\"></i>\n");
+														echo ("							<h3><a href=\"" . $row["path"] . "\">" . $row["name"] . "</a></h3>\n");
+														echo ("						</div>\n");
+														echo ("					</section>\n");
+													}
+												}
+											}
+											?>				</div>
 			</div>
 		</section>

5684530a51d647e64c2c95fafd795eb454236c5f
Adam Blomfield <adam@blomfield.me>
2025-10-11 21:16:44 -0500

    session

---
+++ html/session/index.php
@@ -0,0 +1,53 @@
+<?php
+$sitesec = "home";
+session_start();
+date_default_timezone_set("America/Chicago");
+
+// Import Settings
+include($_SERVER['DOCUMENT_ROOT'] . "/includes/settings.php");
+
+// Get Login Details
+if (isset($_SESSION['authtoken'])) {
+	$loggedin = True;
+	$displayname = $_SESSION["displayname"];
+	$authtoken = $_SESSION["authtoken"];
+	$orgname = $_SESSION["orgname"];
+} else {
+	$loggedin = False;
+}
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title><?php echo ($sitetitle); ?></title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="/assets/css/main.css" />
+	<link rel="icon" type="image/x-icon" href="/images/icononly_transparent_nobuffer.png">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+</head>
+
+<body class="is-preload">
+	<div id="page-wrapper">
+		<!-- Header -->
+		<?php include($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"); ?>
+		<!-- Main Content -->
+		<?php
+		session_start();
+		echo "<h3> PHP List All Session Variables</h3>";
+		foreach ($_SESSION as $key => $val)
+			echo $key . ": " . $val . "<br/>";
+		?> <!-- Footer -->
+		<?php include($_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"); ?>
+	</div>
+	<!-- Scripts -->
+	<script src="/assets/js/jquery.min.js"></script>
+	<script src="/assets/js/jquery.dropotron.min.js"></script>
+	<script src="/assets/js/browser.min.js"></script>
+	<script src="/assets/js/breakpoints.min.js"></script>
+	<script src="/assets/js/util.js"></script>
+	<script src="/assets/js/main.js"></script>
+</body>
+
+</html>

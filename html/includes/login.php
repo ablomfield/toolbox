ed9c767362e509b00e7d89f16adc4313b4780724
Adam Blomfield <adam@blomfield.me>
2025-10-13 08:29:05 -0500

    login

--- html/includes/login.php
+++ html/includes/login.php
@@ -1,7 +1,7 @@
 		<section class="wrapper style1">
 			<div class="container">
-				<div class="row aln-middle image">
-					<center><a href="<?php echo ($oauth_url); ?>" style="border-bottom: none;"><img width="400" src="/images/signin.png" alt="Sign In with Webex" /></a></center>
+				<div class="row aln-center">
+					<a href="<?php echo ($oauth_url); ?>" style="border-bottom: none;"><img width="400" src="/images/signin.png" alt="Sign In with Webex" /></a>
 				</div>
 			</div>
 		</section>

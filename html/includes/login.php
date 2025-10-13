9e24bb3e36d31c15680e92338199ef73f6568845
Adam Blomfield <adam@blomfield.me>
2025-10-13 08:13:07 -0500

    login

--- html/includes/login.php
+++ html/includes/login.php
@@ -3,8 +3,7 @@
 				<div class="row gtr-200">
 					<section class="col-6 col-12-narrower">
 						<div class="box highlight">
-							<a href="<?php echo($oauth_url); ?>"><i class="icon major solid fa-network-wired" style="text-decoration: none;"></i>
-							<h3>Login</h3></a>
+							<a href="<?php echo($oauth_url); ?>"><img width="400" src="/images/signin.png" alt="Sign In with Webex" /></a>
 						</div>
 					</section>
 				</div>
